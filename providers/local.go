43107812c37a8c9d9c2802bc2a50316988990ec6
omeid <public@omeid.me>
2014-11-06 05:21:10 -0500

    Update local provider.

--- providers/local.go
+++ providers/local.go
@@ -10,7 +10,7 @@ import (
 )
 
 func init() {
-	//List["local"] = &Local{}
+	List["local"] = &Local{}
 }
 
 //				 Name     Versions
@@ -27,32 +27,24 @@ func (p Local) Location() string {
 	return p.location
 }
 
-func (p *Local) Packages() ([]string, error) {
+func (p *Local) Update() error {
+	return nil //Error provider Local doesn't support update?
+}
+func (p *Local) Packages(filter string) ([]string, error) {
 	return nil, nil
 }
 
-func (p *Local) Versions() ([]string, error) {
+func (p *Local) Versions(product string) ([]string, error) {
 
 	versions := []string{}
 
 	return versions, nil
 }
 
-func (p *Local) Get(name string, ranges string) (*semver.Version, error) {
-
-	versions, err := p.Versions()
-	if err != nil {
-		return nil, err
-	}
-
-	c, err := semver.NewCollection(versions)
-	if err != nil {
-		return nil, err
-	}
-
-	return c.Latest(ranges)
-}
-
 func (p *Local) Fetch(ver *semver.Version) (io.Reader, error) {
 	return os.Open(path.Join(p.location, flp.Tag(ver)))
+}
+
+func (p *Local) Source(ver *semver.Version) (string, error) {
+	return "", nil
 }
